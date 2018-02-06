9a17379def7096e40bdcb4b23d859c85e1212fb4
alexbollbach <alexander.bollbach@gmail.com>
2018-02-05 23:44:38 -0500

    first

---
+++ src/main.js
@@ -0,0 +1,33 @@
+class CanvasManager {
+	constructor(canvasId) {
+		this.canvas = document.getElementById(canvasId);
+
+		this.context = this.canvas.getContext("2d");
+
+		this.canvas.width = this.context.width;
+		this.canvas.height = this.context.height;
+
+		this.resizeCanvasToDisplaySize(this.canvas);
+	}
+
+	resizeCanvasToDisplaySize(canvas) {
+		const width = canvas.clientWidth;
+		const height = canvas.clientHeight;
+
+		canvas.width = width;
+		canvas.height = height;
+	}
+}
+
+window.onload = function() {
+	startGame();
+};
+
+function startGame() {
+	let canvasManager = new CanvasManager("GameEngineCanvas");
+
+	canvasManager.context.lineWidth = "2";
+	canvasManager.context.strokeStyle = "purple";
+	canvasManager.context.rect(20, 20, 150, 100);
+	canvasManager.context.stroke();
+}
